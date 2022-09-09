968717236a3ed85fa8709f71282f1445787dd00a
Maximiano Eduardo <edu.maxi.96@gmail.com>
2022-09-09 08:50:06 -0300

    :art: Corrigindo Model

--- lib/models/user_models.dart
+++ lib/models/user_models.dart
@@ -1,48 +1,35 @@
 import 'dart:convert';
 
-List<User> userFromMap(String str) =>
+List<User> welcomeFromMap(String str) =>
     List<User>.from(json.decode(str).map((x) => User.fromMap(x)));
 
-String userToMap(List<User> data) =>
+String welcomeToMap(List<User> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toMap())));
 
 class User {
   User({
-    required this.createdAt,
-    required this.name,
-    required this.avatar,
-    required this.id,
     required this.amount,
     required this.transactions,
+    required this.id,
     required this.card,
   });
 
-  final DateTime createdAt;
-  final String name;
-  final String avatar;
-  final String id;
   final String amount;
-  final List<Transaction> transactions;
+  final Transactions transactions;
+  final String id;
   final Card card;
 
   factory User.fromMap(Map<String, dynamic> json) => User(
-        createdAt: DateTime.parse(json["createdAt"]),
-        name: json["name"],
-        avatar: json["avatar"],
-        id: json["id"],
         amount: json["amount"],
-        transactions: List<Transaction>.from(
-            json["transactions"].map((x) => Transaction.fromMap(x))),
+        transactions: Transactions.fromMap(json["transactions"]),
+        id: json["id"],
         card: Card.fromMap(json["card"]),
       );
 
   Map<String, dynamic> toMap() => {
-        "createdAt": createdAt.toIso8601String(),
-        "name": name,
-        "avatar": avatar,
-        "id": id,
         "amount": amount,
-        "transactions": List<dynamic>.from(transactions.map((x) => x.toMap())),
+        "transactions": transactions.toMap(),
+        "id": id,
         "card": card.toMap(),
       };
 }
@@ -67,26 +54,30 @@ class Card {
       };
 }
 
-class Transaction {
-  Transaction({
+class Transactions {
+  Transactions({
     required this.transactionType,
     required this.transactionDescription,
     required this.transactionAmount,
+    required this.date,
   });
 
   final String transactionType;
   final String transactionDescription;
   final String transactionAmount;
+  final DateTime date;
 
-  factory Transaction.fromMap(Map<String, dynamic> json) => Transaction(
+  factory Transactions.fromMap(Map<String, dynamic> json) => Transactions(
         transactionType: json["transactionType"],
         transactionDescription: json["transactionDescription"],
         transactionAmount: json["transactionAmount"],
+        date: DateTime.parse(json["date"]),
       );
 
   Map<String, dynamic> toMap() => {
         "transactionType": transactionType,
         "transactionDescription": transactionDescription,
         "transactionAmount": transactionAmount,
+        "date": date.toIso8601String(),
       };
 }
